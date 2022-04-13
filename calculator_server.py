758ff83a43a9ba8ab53ca613f5ea6bd2a5b82b09
JasonJungler <jasons10219@gmail.com>
2022-04-14 00:10:44 +0800

    first commit

---
+++ calculator_server.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Server of 4 process
+
+This model contains a program that behave as below. 
+    * Server:
+        Accept user keyboard input. The user types integers and separate them by using [Space]. 
+        After clicking [Enter], server will write data to each clients via a socket, pipe, or the shared memory, respectively.
+        
+Example:
+    $ python calculator_server.py
+
+.. _Assessment link:
+   https://hackmd.io/@yS0Jnn7dScOOlYihOAbDfg/rkOZiLJ5Y
+"""
+
+def main():
+    """Generate all processes and """
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ client1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Client1 of 4 process
+
+This model contains a program that behave as below. 
+    * Client1:
+        Reads intergers from the socket and calculate the Mean value of the integers.
+
+Example:
+    $ python calculator_server.py
+
+.. _Assessment link:
+   https://hackmd.io/@yS0Jnn7dScOOlYihOAbDfg/rkOZiLJ5Y
+"""
+def main():
+    """Start process"""
+    pass
+
+if __name__ == '__main__':
+    main()
